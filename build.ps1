1664efc4f1769cd8b618538445798bd9e69d3032
Sidoine De Wispelaere <sidoine@sidoine.net>
2016-07-21 13:50:42 +0200

    Fix package name

--- build.ps1
+++ build.ps1
@@ -12,7 +12,7 @@ if ($version -ne "") {
     & dotnet restore
     cd .\src\Folke.Identity.Server
     & dotnet pack -c Release
-    $file = Get-Item "bin\Release\*-$version.nupkg"
+    $file = Get-Item "bin\Release\*.$version.nupkg"
     nuget push $file.FullName $key -Source https://api.nuget.org/v3/index.json
     cd ..\..
 }
