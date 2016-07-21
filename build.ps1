2ef2b1ad996c969e88e5cc9ad493f88204e5c243
Sidoine De Wispelaere <sidoine@sidoine.net>
2016-07-21 13:27:31 +0200

    Use tags for version numbers

--- build.ps1
+++ build.ps1
@@ -1,10 +1,18 @@
-param([String]$key)
+param([String]$key,[String]$version = $null)
 
-$span = New-TimeSpan -Start 2010/1/1 -End (Get-Date)
-$version = 'beta-' + $span.Days.ToString() + ($span.Hours * 60 + $span.Minutes).ToString("0000")
-& dotnet restore
-cd .\src\Folke.Identity.Server
-& dotnet pack --version-suffix $version
-$file = Get-Item "bin\Debug\*-$version.nupkg"
-nuget push $file.FullName $key -Source https://api.nuget.org/v3/index.json
-cd ..\..
+function setProjectionVersion([String]$fileName, [String]$version) {
+    $content = (Get-Content $fileName) -join "`n" | ConvertFrom-Json
+    $content.version = $version
+    $newContent = ConvertTo-Json -Depth 10 $content
+    Set-Content $fileName $newContent
+}
+
+if ($version -ne $null) {
+    setProjectionVersion ".\src\Folke.Identity.Server\project.json" $version
+    & dotnet restore
+    cd .\src\Folke.Identity.Server
+    & dotnet pack -c Release
+    $file = Get-Item "bin\Release\*-$version.nupkg"
+    # nuget push $file.FullName $key -Source https://api.nuget.org/v3/index.json
+    cd ..\..
+}
