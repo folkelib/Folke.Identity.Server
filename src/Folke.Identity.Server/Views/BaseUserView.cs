53f37b0841e9a25b3780f24093b3949ca570268c
Sidoine De Wispelaere <sidoine@sidoine.net>
2016-08-12 23:19:21 +0200

    Add required on viewmodel

--- src/Folke.Identity.Server/Views/BaseUserView.cs
+++ src/Folke.Identity.Server/Views/BaseUserView.cs
@@ -1,10 +1,14 @@
-﻿namespace Folke.Identity.Server.Views
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Folke.Identity.Server.Views
 {
     public class BaseUserView<TKey>
     {
+        [Required]
         public string UserName { get; set; }
         public bool Logged { get; set; }
         public bool EmailConfirmed { get; set; }
+        [Required]
         public string Email { get; set; }
         public TKey Id { get; set; }
         public bool HasPassword { get; set; }
