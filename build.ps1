1673dbf63b82fdfb210885ddf3dfc329490303ce
Arnaud Castaner <arnaud.castaner@outlook.com>
2017-05-14 18:13:59 +0200

    Update CI script

--- build.ps1
+++ build.ps1
@@ -1,18 +1,27 @@
 param([String]$key,[String]$version)
 
-function setProjectVersion([String]$fileName, [String]$version) {
+param([String]$key,[String]$version)
+
+function setProjectVersion([String]$project, [String]$version) {
+	$fileName =  ".\src\$project\$project.xproj"
     $content = (Get-Content $fileName) -join "`n" | ConvertFrom-Json
     $content.version = $version
     $newContent = ConvertTo-Json -Depth 10 $content
     Set-Content $fileName $newContent
 }
 
+function publishProject([String]$project,[String]$version) {
+	cd ".\src\$project"
+	& dotnet pack -c Release
+	$file = Get-Item "bin\Release\*.$version.nupkg"
+	nuget push $file.FullName $key -Source https://api.nuget.org/v3/index.json
+	cd ..\..
+}
+
 if ($version -ne "") {
-    setProjectVersion ".\src\Folke.Identity.Server\project.json" $version
-    & dotnet restore
-    cd .\src\Folke.Identity.Server
-    & dotnet pack -c Release
-    $file = Get-Item "bin\Release\*.$version.nupkg"
-    nuget push $file.FullName $key -Source https://api.nuget.org/v3/index.json
-    cd ..\..
+	setProjectVersion "Folke.Identity.Server" $version
+	
+	& dotnet restore
+
+	publishProject "Folke.Identity.Server" $version
 }
