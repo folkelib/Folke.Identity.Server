6062a1a66569a28d120ca85b5c5d6640a720689f
Sidoine De Wispelaere <sidoine@sidoine.net>
2017-11-04 18:59:11 +0100

    Role name is required

--- src/Folke.Identity.Server/Views/BaseRoleView.cs
+++ src/Folke.Identity.Server/Views/BaseRoleView.cs
@@ -1,8 +1,11 @@
-﻿namespace Folke.Identity.Server.Views
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Folke.Identity.Server.Views
 {
     public class BaseRoleView<TKey>
     {
         public TKey Id { get; set; }
+        [Required]
         public string Name { get; set; }
     }
 }
